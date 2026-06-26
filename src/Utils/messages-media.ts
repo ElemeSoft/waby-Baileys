deaf67b0487dc64bcb7fe76bace6a569edb98f6e
Luis <sinies7ro@hotmail.com>
2026-06-26 19:28:47 -0300

    refactor(media): simplify MEDIA_RETRY_STATUS_MAP declaration

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -990,11 +990,12 @@ export const decryptMediaRetryData = (
 	return proto.MediaRetryNotification.decode(plaintext)
 }
 
+const MEDIA_RETRY_STATUS_MAP: Record<number, number> = {
+    [proto.MediaRetryNotification.ResultType.SUCCESS]: 200,
+    [proto.MediaRetryNotification.ResultType.DECRYPTION_ERROR]: 412,
+    [proto.MediaRetryNotification.ResultType.NOT_FOUND]: 404,
+    [proto.MediaRetryNotification.ResultType.GENERAL_ERROR]: 418,
+}
+
 export const getStatusCodeForMediaRetry = (code: number) => MEDIA_RETRY_STATUS_MAP[code]
 
-const MEDIA_RETRY_STATUS_MAP = {
-	[proto.MediaRetryNotification.ResultType.SUCCESS]: 200,
-	[proto.MediaRetryNotification.ResultType.DECRYPTION_ERROR]: 412,
-	[proto.MediaRetryNotification.ResultType.NOT_FOUND]: 404,
-	[proto.MediaRetryNotification.ResultType.GENERAL_ERROR]: 418
-} as const
